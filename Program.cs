84ec058bf491e3cf9b9a3794705d282b91259a81
Zer0Tym3 <zchry.mrnng@gmail.com>
2023-07-08 21:41:53 -0400

    e

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿
-
+﻿using System.Text;
 
 class FeistelCipher
 {
+    public static Encoding ascii = Encoding.ASCII;
     static void Main(String[] args)
     {
 
@@ -10,5 +10,13 @@ class FeistelCipher
     private static int randomKey(int keyLength)
     {
         return 0;
+    }
+    private static List<String> strToBin (string input)
+    {
+        List<char> tempC = new List<char>(input.ToCharArray());
+        List<Byte> tempB = new List<Byte>(ascii.GetBytes(tempC.ToArray()));
+        List<string> binOut = new List<string>(tempB.ToArray().Select(x => Convert.ToString(x, 2).PadLeft(8, '0')).ToArray());
+        return binOut;
+    
     }
 }
